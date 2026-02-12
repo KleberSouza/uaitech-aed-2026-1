2f01b219194b9957adb160afc628f892e6d98c10
KleberSouza <kleberjfsouza@gmail.com>
2026-02-12 20:33:58 -0300

    aula-12-02-26-categoria

---
+++ Categoria.cs
@@ -0,0 +1,72 @@
+﻿namespace uaitech;
+
+class Categoria
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public void Imprimir()
+    {
+        Console.WriteLine($"{Id} - {Nome}");
+    }
+}
+
+class Categorias
+{
+    private Categoria[] lista;
+    private int n;
+    private int MAX;
+
+    public Categorias(int max)
+    {
+        MAX = max;
+        lista = new Categoria[max];
+        n=0;
+    }
+
+    public void Adicionar()
+    {
+        if(n == MAX)
+        {
+            Console.WriteLine("Lista cheia! :( )");            
+        }else{
+            Categoria categoria = new Categoria();
+            Console.WriteLine("Digite o Id:");
+            categoria.Id = int.Parse(Console.ReadLine());
+
+            Categoria cat1 = PesquisaPorId(categoria.Id);
+
+            if (cat1 != null)
+            {
+                Console.WriteLine("Id já cadastrado!");
+                cat1.Imprimir();
+                return;
+            }
+
+            Console.WriteLine("Digite o Nome:");
+            categoria.Nome = Console.ReadLine();
+
+            lista[n] = categoria;
+            n++;
+        }
+
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine($"Categorias: {n}");
+        for(int i=0; i < n; i++)
+            lista[i].Imprimir();
+    }
+    
+
+    public Categoria PesquisaPorId(int id)
+    {
+        for(int i=0; i < n; i++)
+        {
+            if(lista[i].Id == id)
+                return lista[i];
+        }
+        return null;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace uaitech;
+
+class Program
+{
+
+    static int Menu()
+    {
+        Console.WriteLine("===> UAI TECH <===");
+        Console.WriteLine("1 - Adicionar Categoria");
+        Console.WriteLine("2 - Listar Categorias");
+        Console.WriteLine("0 - Sair");
+        int op = int.Parse(Console.ReadLine());
+        return op;
+    }
+
+
+    static void Main(string[] args)
+    {
+        int opcao = 0;
+        Categorias categorias = new Categorias(5);
+
+        do
+        {
+            Console.Clear();
+            opcao = Menu();
+
+            switch (opcao)
+            {
+                case 1: categorias.Adicionar();
+                break;
+                case 2: categorias.Imprimir();
+                break;
+            }
+        Console.ReadKey();
+            
+        }while(opcao != 0);
+    }
+}
