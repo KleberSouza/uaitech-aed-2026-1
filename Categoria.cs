22551f1758b3b912a4e7554a17b747e54bd27c53
KleberSouza <kleberjfsouza@gmail.com>
2026-02-19 20:16:52 -0300

    aula-19-02-26-crud-categoria

--- Categoria.cs
+++ Categoria.cs
@@ -31,7 +31,7 @@ class Categorias
             Console.WriteLine("Lista cheia! :( )");            
         }else{
             Categoria categoria = new Categoria();
-            Console.WriteLine("Digite o Id:");
+            Console.Write("Digite o Id:");
             categoria.Id = int.Parse(Console.ReadLine());
 
             Categoria cat1 = PesquisaPorId(categoria.Id);
@@ -43,7 +43,7 @@ class Categorias
                 return;
             }
 
-            Console.WriteLine("Digite o Nome:");
+            Console.Write("Digite o Nome:");
             categoria.Nome = Console.ReadLine();
 
             lista[n] = categoria;
@@ -58,7 +58,60 @@ class Categorias
         for(int i=0; i < n; i++)
             lista[i].Imprimir();
     }
+
+    public void Editar()
+    {
+        Console.Write("Digite o Id:");
+        int id = int.Parse(Console.ReadLine());
+
+        Categoria cat1 = PesquisaPorId(id);
+
+        if (cat1 != null)
+        {
+            Console.Write("Digite o Nome:");
+            cat1.Nome = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Id não encontrado!");
+        }        
+    }
+
+    public void Excluir()
+    {
+        Console.Write("Digite o Id:");
+        int id = int.Parse(Console.ReadLine());
+
+        int index = PesquisarIndexPorId(id);
+
+        if (index != -1)
+        {
+            n--;
+            for(int i=index; i<n;i++)
+                lista[i] = lista[i+1];
+        }
+        else
+        {
+            Console.WriteLine("Id não encontrado!");
+        }        
+    }
     
+    public void Pesquisar()
+    {
+        Console.Write("Digite o Id:");
+        int id = int.Parse(Console.ReadLine());
+
+        Categoria cat1 = PesquisaPorId(id);
+
+        if (cat1 != null)
+        {
+            cat1.Imprimir();
+        }
+        else
+        {
+            Console.WriteLine("Id não encontrado!");
+        }        
+    }
 
     public Categoria PesquisaPorId(int id)
     {
@@ -68,5 +121,15 @@ class Categorias
                 return lista[i];
         }
         return null;
+    }
+
+    public int PesquisarIndexPorId(int id)
+    {
+        for(int i=0; i < n; i++)
+        {
+            if(lista[i].Id == id)
+                return i;
+        }
+        return -1;
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ class Program
         Console.WriteLine("===> UAI TECH <===");
         Console.WriteLine("1 - Adicionar Categoria");
         Console.WriteLine("2 - Listar Categorias");
+        Console.WriteLine("3 - Editar Categoria");
+        Console.WriteLine("4 - Apagar Categoria");
+        Console.WriteLine("5 - Pesquisar Categoria");
         Console.WriteLine("0 - Sair");
+        Console.Write("Digite a opção desejada: ");
         int op = int.Parse(Console.ReadLine());
         return op;
     }
@@ -30,7 +34,15 @@ class Program
                 break;
                 case 2: categorias.Imprimir();
                 break;
+                case 3: categorias.Editar();
+                break;
+                case 4: categorias.Excluir();
+                break;
+                case 5: categorias.Pesquisar();
+                break;
             }
+
+        Console.WriteLine("\nDigite qualquer tecla para continuar...");
         Console.ReadKey();
             
         }while(opcao != 0);
